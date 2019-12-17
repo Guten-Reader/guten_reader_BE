77ce6a2e537d28c9a24f6808899323e9f0e155d3
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-17 07:18:04 -0700

    Update with flask_restful api

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello():
-    return 'Guten-Reader API'
+# @app.route('/')
+# def hello():
+#     return 'Guten-Reader API'
+
+class User(Resource):
+    def get(self, name):
+        return {'user': name}
+
+api.add_resource(User, '/users/<string:name>')
 
 if __name__ == '__main__':
     app.run(port=5000)
