e9bc3f03f53cc396d61ba3b8a335e0b9b1cc3890
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-17 12:56:46 -0700

    Intial setup for flask with postgres database, POST USER working, GET USER not working

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Api
+from resources.user import User
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/guten_reader_dev'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 api = Api(app)
 
-# @app.route('/')
-# def hello():
-#     return 'Guten-Reader API'
+# flask migrate?
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
-class User(Resource):
-    def get(self, name):
-        return {'user': name}
-
-api.add_resource(User, '/users/<string:name>')
+api.add_resource(User, '/users')
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
---
+++ resources/user.py
@@ -0,0 +1,28 @@
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+
+class User(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank."
+                        )
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank."
+                        )
+    def get(self, _id):
+        user = UserModel.find_by_id(_id)
+        if user:
+            return user.json()
+
+    def post(self):
+        data = User.parser.parse_args()
+        if UserModel.find_by_username(data['username']):
+            return {"message": "A user with that username already exists"}, 400
+
+        user = UserModel(data['username'], data['password'])
+        user.save_to_db()
+        return {"message": "User created successfully."}, 201
