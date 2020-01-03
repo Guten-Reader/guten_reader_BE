04ce358b2a4bdf7214c301bfc17797613a594137
MackHalliday <mackenzie.halliday@gmail.com>
2020-01-03 00:06:49 -0700

    pseudo code, updated args for recommendation method, update varible for converting sentiment value

--- services/spotify_service.py
+++ services/spotify_service.py
@@ -1,12 +1,10 @@
 class SpotifyService:
-
     def spotify_sentiment_value(google_lang_sentiment_value):
         converted_value = (sentiment_value + 1)/2
         return converted_value
 
-    def recommendation(google_lang_sentiment_value, user_id, access_token):
-        convert_sentiment_value(senitment_value)
-         # spotify_sentiment_value
+    def recommendation(access_token, user_id, sentiment_value):
+        valence = spotify_sentiment_value(senitment_value)
          # GET https://api.spotify.com/v1/recommendations?seed_genres=classical&limit=1&valence=[SPOTIFY_SENTIMENT_VALUE]
          # Needs Authorization in header
          # if Authorization success
@@ -18,4 +16,4 @@ class SpotifyService:
             # retry Spotify recommendation API call
             # parse JSOn for track ID
             # return track ID
-        return track_ID
+        return track_id
