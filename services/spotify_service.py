9dcaf1869f3ec3e98eba40fc067e7b7f615e0667
MackHalliday <mackenzie.halliday@gmail.com>
2020-01-03 09:23:47 -0700

    Minor update to varible name, additional comments regarding refresh logic

--- services/spotify_service.py
+++ services/spotify_service.py
@@ -8,7 +8,7 @@ class SpotifyService:
         converted_value = (sentiment_value + 1)/2
         return round(converted_value, 1)
 
-    def recommend(self, access_token, user_id, sentiment_value):
+    def recommend_track(self, access_token, user_id, sentiment_value):
         valence = self.spotify_sentiment_value(sentiment_value)
         params = {'valence': valence,
                   'seed_genres': 'classical',
@@ -18,8 +18,12 @@ class SpotifyService:
         body = request.json()
         return body['tracks'][0]['id']
 
-        # if access token invalid
+        # if access token invalid then...
             # make GET request to rails app to get new access token
-            # retry Spotify recommendation API call
-            # parse JSON for track ID
-            # return track ID
+            # could user user_id or maybe access_token to determine which access_token to update
+            # once access_token updated, retries Spotify recommendation API call above
+            # if second Spotify recommedation API sucessful then...
+                # parse JSON for track ID
+                # return track ID
+            # if second Spotify recommendation API fail then ...
+                # send message call could not be completed
