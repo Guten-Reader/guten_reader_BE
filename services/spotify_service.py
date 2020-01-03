a432a814e30b0d0ee7824a44dfd5d9a1e8c3d502
MackHalliday <mackenzie.halliday@gmail.com>
2020-01-02 17:22:06 -0700

    General layout for spotify service

--- services/spotify_service.py
+++ services/spotify_service.py
@@ -1,3 +1,9 @@
 class SpotifyService:
-    def hello():
-        return "chicken"
+
+    def convert_sentiment_value(sentiment_value):
+        converted_value = (sentiment_value + 1)/2
+        return converted_value
+    def recommendation(sentiment_value):
+        convert_sentiment_value(senitment_value)
+         #GET https://api.spotify.com/v1/recommendations
+        return track_number
