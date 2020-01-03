a16c421fadcc3f8aa384a4f88949f6950f7e20e4
MackHalliday <mackenzie.halliday@gmail.com>
2020-01-02 23:43:52 -0700

    pseudo code for recommendation api call with access token refresh

--- services/spotify_service.py
+++ services/spotify_service.py
@@ -1,9 +1,21 @@
 class SpotifyService:
 
-    def convert_sentiment_value(sentiment_value):
+    def spotify_sentiment_value(google_lang_sentiment_value):
         converted_value = (sentiment_value + 1)/2
         return converted_value
-    def recommendation(sentiment_value):
+
+    def recommendation(google_lang_sentiment_value, user_id, access_token):
         convert_sentiment_value(senitment_value)
-         #GET https://api.spotify.com/v1/recommendations
-        return track_number
+         # spotify_sentiment_value
+         # GET https://api.spotify.com/v1/recommendations?seed_genres=classical&limit=1&valence=[SPOTIFY_SENTIMENT_VALUE]
+         # Needs Authorization in header
+         # if Authorization success
+            # parse JSON for track ID
+            # return track ID
+
+         # if Authorization fails
+            # make GET request to rails app to get new access token
+            # retry Spotify recommendation API call
+            # parse JSOn for track ID
+            # return track ID
+        return track_ID
