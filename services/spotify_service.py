c93426d3450e011b33786bbdd726bec3bfcf8cb5
fentontaylor <fentontaylor@gmail.com>
2020-01-08 09:16:54 -0700

    Refactor SpotifyService to use more helper functions and initialize with access_token and sentiment
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- services/spotify_service.py
+++ services/spotify_service.py
@@ -1,42 +1,61 @@
 import requests
 
 class SpotifyService:
+    def __init__(self, access_token, sentiment):
+        self.access_token = access_token
+        self.sentiment = sentiment
 
-    def recommend(self, access_token, sentiment):
-        spotify_recommendation = self.get_spotify_recommendation(access_token, sentiment)
 
+    def recommend(self):
+        spotify_recommendation = self.get_spotify_recommendation()
         if spotify_recommendation.status_code == 200:
             body = spotify_recommendation.json()
-        
-            track_list = []
-            for item in body['tracks']:
-                track_list.append(item['uri'])
-            print(track_list)
-
-            return { 'result': { 'recommended_tracks': track_list, 'mood': sentiment}, 'status_code': 200 }
+            return self._recommended_tracks(body)
         elif spotify_recommendation.status_code == 401:
-            return { 'result': { 'message': "The access token expired"}, 'status_code': 401}
+            return self._expired_token()
         else:
-            return { 'result': { 'message': "Bad request"}, 'status_code': 400 }
+            return self._bad_request()
 
-    def song_params(self, sentiment):
+
+    def song_params(self):
         params = {
             'seed_genres': 'classical',
-            'limit': 10,
-            }
-        if sentiment == 'Positive':
-            params['mode'] = 1
+            'limit': 10 }
+        if self.sentiment == 'Positive':
+            params['mode']    = 1
             params['valence'] = 1
-        elif sentiment == 'Negative':
-            params['mode'] = 0
+        elif self.sentiment == 'Negative':
+            params['mode']    = 0
             params['valence'] = 0
         else:
             params['valence'] = 0.5
         return params
 
 
-    def get_spotify_recommendation(self, access_token, sentiment_value):
-        params = self.song_params(sentiment_value)
-        headers = {'Authorization': f'Bearer {access_token}'}
-        request = requests.get('https://api.spotify.com/v1/recommendations', headers=headers, params=params)
-        return request
+    def get_spotify_recommendation(self):
+        params = self.song_params()
+        headers = {'Authorization': f'Bearer {self.access_token}'}
+        response = requests.get(
+            'https://api.spotify.com/v1/recommendations',
+            headers=headers,
+            params=params)
+        return response
+
+
+    def _bad_request(self):
+        return { 'message': 'Bad request', 'status_code': 400 }
+    
+
+    def _expired_token(self):
+        return { 'message': 'The access token expired', 'status_code': 401}
+    
+
+    def _recommended_tracks(self, body):
+        track_list = []
+        for item in body['tracks']:
+            track_list.append(item['uri'])
+        return {
+            'recommended_tracks': track_list,
+            'mood': self.sentiment,
+            'status_code': 200
+        }
