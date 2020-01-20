dbc04626726bafc8e01ed6d66b862fc6b1cbf326
MackHalliday <mackenzie.halliday@gmail.com>
2020-01-19 19:18:34 -0700

    Update with MagicMock, failing when converting watson value to Spotify valence value

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,12 @@
 import unittest
 import json
+import os
 from unittest.mock import patch
+from mock import MagicMock
 from app import app
 from services.monkeylearn_service import MonkeyLearnService
 from services.watson_service import WatsonService
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from services.spotify_service import SpotifyService
 
 class TestHello(unittest.TestCase):
@@ -31,56 +34,60 @@ class TestHello(unittest.TestCase):
                                 data=json.dumps(data),
                                 content_type='application/json')
 
+
         self.assertEqual(200, response.status_code)
         self.assertDictEqual(ml_data[0], response.json[0])
 
-    @patch.object(WatsonService, 'get_sentiment')
+
+    @patch.object(WatsonService, 'get_watson_text_analyze')
     def test_get_watson(self, mock_text_sentiment):
         file_path = 'tests/fixtures/watson_positive.json'
         with open(file_path) as json_file:
             ml_data = json.load(json_file)
 
-        mock_text_sentiment.return_value = ml_data
+        # mock_text_sentiment.return_value = ml_data
+        # import code; code.interact(local=dict(globals(), **locals()))
+        mock_text_sentiment.return_value = MagicMock(status_code=200,response=ml_data)
+
 
         data = {
             'text': "after signalling to him to stop:\r\n\r\n'Tell me, Johann, what is tonight?'\r\n\r\nHe crossed himself, as he answered laconically: 'Walpurgis nacht.' Then he took out his watch, a great, old-fashioned German silver thing as big as a turnip, and looked at it, with his eyebrows gathered together and a little impatient shrug of his shoulders. I realised that this was his way of respectfully protesting against the unnecessary delay, and sank back in the carriage, merely motioning him to proceed. He started off rapidly, as if to make up for lost time. Every now and then the horses seemed to throw up their"
         }
 
-        response = self.app.get('/api/v1/watson',
-                                data=json.dumps(data),
-                                content_type='application/json')
+        response = self.app.get('/api/v1/watson', data=json.dumps(data), content_type='application/json')
 
         self.assertEqual(200, response.status_code)
-        self.assertDictEqual(ml_data[0], response.json[0])
+        self.assertDictEqual(ml_data, response.json)
+
 
     @patch('services.spotify_service.requests.get')
-    @patch.object(WatsonService, 'get_sentiment')
+    @patch.object(WatsonService, 'get_sentiment_value')
     def test_POST_recommendation(self, mock_text_sentiment, mock_get):
         file_path = 'tests/fixtures/watson_positive.json'
         with open(file_path) as json_file:
             ml_data = json.load(json_file)
 
         mock_text_sentiment.return_value = ml_data
+        # mock_text_sentiment.return_value.status_code = 200
 
         file_path = 'tests/fixtures/spotify_tracks.json'
         with open(file_path) as json_file:
             tracks_data = json.load(json_file)
 
-        mock_get.return_value.status_code = 200
         mock_get.return_value.json.return_value = tracks_data
 
         data = {
             'text': "after signalling to him to stop:\r\n\r\n'Tell me, Johann, what is tonight?'\r\n\r\nHe crossed himself, as he answered laconically: 'Walpurgis nacht.' Then he took out his watch, a great, old-fashioned German silver thing as big as a turnip, and looked at it, with his eyebrows gathered together and a little impatient shrug of his shoulders. I realised that this was his way of respectfully protesting against the unnecessary delay, and sank back in the carriage, merely motioning him to proceed. He started off rapidly, as if to make up for lost time. Every now and then the horses seemed to throw up their",
-            'current_mood': 'Negative',
+            'current_mood': 0,
             'access_token': 'totally-real-access-token'
         }
 
         response = self.app.post('/api/v1/recommendation',
                                 data=json.dumps(data),
                                 content_type='application/json')
-
+        import code; code.interact(local=dict(globals(), **locals()))
         expected = {
-            'mood': 'Positive',
+            'mood': 1,
             'recommended_tracks': [
                 'spotify:track:6PrKZUXJPmBiobMN44yR8Y',
                 'spotify:track:1IM8x4lxZVOOP9gpQD6c5s',
@@ -95,11 +102,12 @@ class TestHello(unittest.TestCase):
             'status_code': 200}
 
         self.assertEqual(200, response.status_code)
+
         self.assertDictEqual(expected, response.json)
 
         new_data = {
             'text': 'Super positive great happy fun times fun happy awesome',
-            'current_mood': 'Positive',
+            'current_mood': 1,
             'access_token': 'totally-real-access-token'
         }
 
